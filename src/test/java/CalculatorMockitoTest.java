16b80543cea506074a0ba3ad645fe50de919ef2c
Ronak Mistry <ronak@bluefinenterprises.com>
2018-08-27 14:30:30 -0400

    JUNIT 5 maven dependency added, BiFunction simple testing done

--- src/test/java/CalculatorMockitoTest.java
+++ src/test/java/CalculatorMockitoTest.java
@@ -1,20 +1,38 @@
-import main.java.Calculator;
-import org.junit.Test;
+//import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.function.BiFunction;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-
+@ExtendWith(MockitoExtension.class)
 public class CalculatorMockitoTest {
 
     @Mock
-    Calculator calculator;
+    private BiFunction<Integer, Integer, Integer> functionOne;
+
+    @Mock
+    private BiFunction<Integer, Integer, Integer> functionTwo;
 
     @Test
     public void functionOneTestSimple() {
-        when(calculator.functionOne(10,20)).thenReturn(30);
+        when(functionOne.apply(any(), any())).thenReturn(5);
+        System.out.println(functionOne.apply(1, 2));
+    }
+
+    @Test
+    public void functionOneVerify() {
+        verify(functionOne.apply(any(), any()).toString());
+    }
+
+    @Test
+    public void functionTwiTestSimple() {
+        when(functionTwo.apply(any(), any())).thenReturn(5);
+        System.out.println(functionTwo.apply(1, 2));
     }
 }
